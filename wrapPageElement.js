ccfc49d7b0b675ad1ab74eb0de007e849c867c36
John Parnefjord <john.parnefjord@gmail.com>
2020-02-28 12:17:40 +0100

    Update wrapPageElement.js

--- wrapPageElement.js
+++ wrapPageElement.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Layout } from './src/components/common/Layout';
-// import { useIntl } from 'gatsby-plugin-intl';
 
 // Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 const wrapPageElement = ({ element, props }) => {
-  // const intl = useIntl();
   return <Layout {...props}>{element}</Layout>;
 };
 
